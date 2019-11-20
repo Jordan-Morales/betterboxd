e4dff0c2841b4d12fb2dffcbebd03c0a343aca22
Alex Merced - AlexMercedCoder.com <alexmerced@alexmerced.com>
2019-11-20 16:20:35 -0500

    user controller

---
+++ controllers/usersController.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const session = require('express-session');
+
+const Users = require('../models/users.js');
+
+router.get('/', (req, res)=>{
+    Users.find({}, (err, foundUsers)=>{
+        res.json(foundUsers);
+    });
+});
+
+router.post('/', (req, res)=>{
+    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+    Users.create(req.body, (err, createdUser)=>{
+        res.status(201).json({
+            status:201,
+            message: "user created"
+        });
+    });
+});
+
+
+router.delete('/:id', (req, res)=>{
+    Users.findByIdAndRemove(req.params.id, (err, deletedUser)=>{
+        res.json(deletedUser);
+
+    });
+});
+
+router.put('/:id', (req, res)=>{
+    Users.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedUser)=>{
+        res.json(updatedUser);
+    });
+});
+
+module.exports = router;
