db168b8e33b1b207a5da857d7a18a7444b3af899
Jordan Morales <jordan.morales@gmail.com>
2019-11-20 19:45:06 -0800

    pesudocoding; mapped out app.js basic features, laidout index.html, movie controller mapped out other functions.

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -6,31 +6,61 @@ const Movie = require('../models/movies.js')
 // INDEX
 
 movies.get('/', (req, res) => {
-    Movie.find({}, (err, foundMovie) => {
-      res.json(foundMovie);
-    });
+  Movie.find({}, (error, foundMovie) => {
+    res.json(foundMovie);
+  });
+});
+
+// SHOW / GET BY ID
+// used to access each movie and the contained comments
+movies.get('/movie/:id', (req, res) => {
+  Movie.findById(req.params.id, (error, foundMovie) => {
+    res.json(foundMovie);
+  });
 });
 
 // CREATE
 movies.post('/', (req, res) => {
-    Movie.create(req.body, (error, createdMovie) => {
-      res.json(createdMovie);
-    });
+  Movie.create(req.body, (error, createdMovie) => {
+    res.json(createdMovie);
+  });
 });
 
 // DELETE
 movies.delete('/:id', (req, res) => {
-    Movie.findByIdAndRemove(req.params.id, (error, deletedMovie) => {
-      res.json(deletedMovie);
-    });
+  Movie.findByIdAndRemove(req.params.id, (error, deletedMovie) => {
+    res.json(deletedMovie);
+  });
 });
 
 // UPDATE
 movies.put('/:id', (req, res) => {
-    Movie.findByIdAndUpdate(req.params.id, req.body, {new:true}, (error, updatedMovie) => {
-      res.json(updatedMovie);
-    });
+  Movie.findByIdAndUpdate(req.params.id, req.body, {new:true}, (error, updatedMovie) => {
+    res.json(updatedMovie);
+  });
 });
+
+
+//// General thought, couldn't these to be merged into one if else statement?
+// LIKE MOVIE
+//find by movie by id . likes $inc +1
+// find user by id . movie array, add object to array
+
+// DISLIKE MOVIE
+// find movie by id, find and search user movie array if contains movies
+// if present like $inc -1, remove from user movie array by ID
+// else do
+
+
+// ADD COMMENT TO MOVIE
+// find by movie by id.commentsArray, take form data from html push to array
+
+// EDIT COMMENT
+// find movie by id, find and search movie comment array if contains comments from user, take and update comment by id
+
+// DELETE COMMENT FROM MOVIE
+// find movie by id, find and search movie comment array if contains comments from user, take and remove comment by id (probably a splice(one) and .join()?)
+
 
 
 module.exports = movies
