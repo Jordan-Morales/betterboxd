b759bbf8c5df05599c8bacee65208b5ce5103839
Alex Merced - AlexMercedCoder.com <alexmerced@alexmerced.com>
2019-11-21 15:39:34 -0500

    profile include and js

---
+++ public/js/profile.js
@@ -0,0 +1,13 @@
+/////////////////////////////////
+//To include in HTML
+//<div ng-include="'includes/profile.html'"></div>
+/////////////////////////////////
+
+
+this.profileOn = false
+
+this.showProfile () => {
+
+    controller.profileOn = !controller.profileOn;
+
+}
